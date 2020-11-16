47ebfbea56e3c36222ccf3b100304d8cd2263d25
acrysense <54480354+acrysense@users.noreply.github.com>
2020-11-16 09:26:56 +0200

     Added new pages

--- js/app.js
+++ js/app.js
@@ -16,23 +16,63 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // TABS
-    document.querySelectorAll('.switch__item').forEach((item, i) => {
-        item.addEventListener('click', () => {
-            document.querySelectorAll('.switch__item').forEach((child) => child.classList.remove('switch__item--active'))
-            document.querySelectorAll('.switch__content').forEach((child) => child.classList.remove('switch__content--active'))
+    const switchItems = document.querySelectorAll('.switch__item'),
+        tabsItems = document.querySelectorAll('.tabs__item'),
+        categoriesItems = document.querySelectorAll('.categories__link')
 
-            item.classList.add('switch__item--active')
-            document.querySelectorAll('.switch__content')[i].classList.add('switch__content--active')
+    if (switchItems) {
+        switchItems.forEach((item, i) => {
+            item.addEventListener('click', () => {
+                document.querySelectorAll('.switch__item').forEach((child) => child.classList.remove('switch__item--active'))
+                document.querySelectorAll('.switch__content').forEach((child) => child.classList.remove('switch__content--active'))
+    
+                item.classList.add('switch__item--active')
+                document.querySelectorAll('.switch__content')[i].classList.add('switch__content--active')
+            })
         })
-    })
+    }
 
-    document.querySelectorAll('.tabs__item').forEach((item, i) => {
-        item.addEventListener('click', () => {
-            document.querySelectorAll('.tabs__item').forEach((child) => child.classList.remove('tabs__item--active'))
-            document.querySelectorAll('.tabs__content').forEach((child) => child.classList.remove('tabs__content--active'))
-
-            item.classList.add('tabs__item--active')
-            document.querySelectorAll('.tabs__content')[i].classList.add('tabs__content--active')
+    if (tabsItems) {
+        tabsItems.forEach((item, i) => {
+            item.addEventListener('click', () => {
+                document.querySelectorAll('.tabs__item').forEach((child) => child.classList.remove('tabs__item--active'))
+                document.querySelectorAll('.tabs__content').forEach((child) => child.classList.remove('tabs__content--active'))
+    
+                item.classList.add('tabs__item--active')
+                document.querySelectorAll('.tabs__content')[i].classList.add('tabs__content--active')
+            })
         })
-    })
+    }
+
+    if (categoriesItems) {
+        categoriesItems.forEach((item, i) => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault()
+                document.querySelectorAll('.categories__link').forEach((child) => child.classList.remove('categories__link--active'))
+                item.classList.add('categories__link--active')
+            })
+        })
+    }
+
+    // ACCORDIONS FAQ
+    const faq = document.querySelectorAll('.faq__trigger')
+
+    if (faq) {
+        $('.faq__trigger').click(function() {
+            const triggerItem = $(this);
+    
+            if (triggerItem.hasClass('faq__trigger--active')) {
+                $(this).removeClass('faq__trigger--active');
+                $(this).next('.faq__content').slideUp(350);
+                console.log('Yes')
+            }
+            else {
+                $('.faq__trigger').removeClass('faq__trigger--active');
+                $('.faq__trigger').next('.faq__content').slideUp(350);
+                $(this).addClass('faq__trigger--active');
+                $(this).next('.faq__content').slideDown(350);
+                console.log('No')
+            }
+        })
+    }
 });
